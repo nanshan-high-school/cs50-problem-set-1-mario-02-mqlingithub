e52b6421e01422c0568942fff4b9eeaf95c6ef96
mqlingithub <43640545+mqlingithub@users.noreply.github.com>
2018-10-25 19:29:02 +0800

    Update Mario02.cpp

--- Mario02.cpp
+++ Mario02.cpp
@@ -4,7 +4,7 @@ int main() {
   int height;
   cout << "請輸入三角形高度(高度勿大於八或小於一):";
   cin >> height;
-  if (9>height && height>0) {
+  if (height<9 && height>0) {
     for (int a=0;a<height;a++) {
       for (int b=height;b>a;b--) {
         cout << " ";
